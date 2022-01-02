5082e6a4ea43d2603dfe1f19c885fc5ff3cf1f27
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2022-01-02 17:32:15 +0530

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -353,9 +353,9 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "Health", url="https://t.me/komiupdates/10"),
+                                "Health", url="https://t.me/ErinaUpdates/3"),
                             InlineKeyboardButton(
-                                "Disaster", url="https://t.me/komiupdates/4")
+                                "Disaster", url="https://t.me/ErinaUpdates/3")
                         ],
                     ]
                 ),
