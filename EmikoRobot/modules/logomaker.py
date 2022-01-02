7a2d5a1614f1e1fea69c9cf3cea2fa323d0963c4
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2022-01-02 13:38:33 +0530

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Komi](https://t.me/KomiXryu_Bot)",
+            caption="Logo by [Erina](https://t.me/Erina_GroupBot)",
             force_document=False,
         )
         os.remove(flnme)
