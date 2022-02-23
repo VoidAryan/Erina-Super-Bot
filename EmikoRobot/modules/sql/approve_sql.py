d723ec244dfe587f55470f106d548d512dbc738a
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2022-02-23 22:01:44 +0530

    Update approve_sql.py

--- EmikoRobot/modules/sql/approve_sql.py
+++ EmikoRobot/modules/sql/approve_sql.py
@@ -1,7 +1,11 @@
 import threading
-from sqlalchemy import Column, String, UnicodeText, func, distinct
+
+from sqlalchemy import Column, String, Integer
+
 from EmikoRobot.modules.sql import BASE, SESSION
 from sqlalchemy.sql.sqltypes import BigInteger
+
+
 
 class Approvals(BASE):
     __tablename__ = "approval"
