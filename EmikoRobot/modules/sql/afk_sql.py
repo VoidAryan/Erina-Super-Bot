5ac32d7665b963aeb65573270304ea8b58ccc70e
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2022-02-23 20:58:12 +0530

    Update afk_sql.py

--- EmikoRobot/modules/sql/afk_sql.py
+++ EmikoRobot/modules/sql/afk_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from EmikoRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
